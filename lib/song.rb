882661a2383061a098bad35c5f439a52a7c10684
Denise Henderson <denise@henderson3.us>
2020-07-28 20:15:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -36,9 +36,44 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == false
-      self.create_by_name(name)
-    end
+    #if self.find_by_name(name) == false
+    #  self.create_by_name(name)
+    #end
+    #binding.pry
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    #binding.pry
+    @@all.sort_by { |song| song.name}
+  end
+
+  def self.new_from_filename(name)
+    #name = ""
+    artist_name = ""
+    temp_name =  ""
+    song = self.new
+    song.artist_name = name.split(" - ")[0].strip
+    temp_name = name.split("-")[1].strip
+    song.name = temp_name.split(".")[0]
+    #binding.pry
+    song
+  end
+
+  def self.create_from_filename(name)
+    #name = ""
+    artist_name = ""
+    temp_name =  ""
+    song = self.new
+    song.artist_name = name.split(" - ")[0].strip
+    temp_name = name.split("-")[1].strip
+    song.name = temp_name.split(".")[0]
+    #binding.pry
+    @@all << song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
