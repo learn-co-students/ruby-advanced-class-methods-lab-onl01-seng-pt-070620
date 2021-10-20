d5feeb2e0373b233ed0ebddbb76d4fcef6139174
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-20 05:46:29 -0400

    use save method i create song

--- lib/song.rb
+++ lib/song.rb
@@ -3,19 +3,19 @@ require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def save
+    self.class.all << self
+  end
   
   def self.create
     song = self.new
-    self.all << song
+    song.save
     song
   end
 
   def self.all
     @@all
-  end
-
-  def save
-    self.class.all << self
   end
   
   def self.new_by_name(name)
